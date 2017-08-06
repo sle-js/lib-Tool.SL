24763e9c89f4501c2c5cf10668186244d5106bd2
Graeme Lockley <graemel@no9.co.za>
2017-08-06 08:59:52 +0200

    chore: add String into Libs

--- src/Libs.js
+++ src/Libs.js
@@ -1,4 +1,5 @@
 module.exports = {
     Array: mrequire("core:Native.Data.Array:1.0.0"),
-    Maybe: mrequire("core:Native.Data.Maybe:1.0.0")
+    Maybe: mrequire("core:Native.Data.Maybe:1.0.0"),
+    String: require("./String")
 };
