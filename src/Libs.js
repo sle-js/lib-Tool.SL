65ea4fdf599f7a846bbe84190f8c3960c4584ffd
Graeme Lockley <graemel@no9.co.za>
2017-08-06 08:16:24 +0200

    refactor: move array from test into src

--- src/Libs.js
+++ src/Libs.js
@@ -1,3 +1,4 @@
 module.exports = {
+    Array: mrequire("core:Native.Data.Array:1.0.0"),
     Maybe: mrequire("core:Native.Data.Maybe:1.0.0")
 };
--- test/Libs.js
+++ test/Libs.js
@@ -3,6 +3,5 @@ const Libs = require("../src/Libs");
 
 module.exports = Object.assign({}, Libs, {
     Assertion: mrequire("core:Test.Unit.Assertion:1.1.0"),
-    Array: mrequire("core:Native.Data.Array:1.0.0"),
     Unit: mrequire("core:Test.Unit:0.0.1")
 });
