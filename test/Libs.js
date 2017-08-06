5da384423bbf0896a09de18c095d39b7d0d69c4e
Graeme Lockley <graemel@no9.co.za>
2017-08-06 07:44:53 +0200

    chore: add array

--- test/Libs.js
+++ test/Libs.js
@@ -3,5 +3,6 @@ const Libs = require("../src/Libs");
 
 module.exports = Object.assign({}, Libs, {
     Assertion: mrequire("core:Test.Unit.Assertion:1.1.0"),
+    Array: mrequire("core:Native.Data.Array:1.0.0"),
     Unit: mrequire("core:Test.Unit:0.0.1")
 });
