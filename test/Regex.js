3d41aa924dfbc1e15f5be06aa90365763324f92d
Graeme Lockley <graemel@no9.co.za>
2017-08-06 07:50:11 +0200

    feature: add Regex helper

---
+++ test/Regex.js
@@ -0,0 +1,32 @@
+const Maybe = require("./Libs").Maybe;
+
+
+function Regex(regex) {
+    this.regex = regex;
+}
+
+
+function from(regex) {
+    return new Regex(regex);
+}
+
+
+Regex.prototype.matchFrom = function (input) {
+    return index => {
+        this.regex.lastIndex = index;
+        const matchedRegex = this.regex.exec(input);
+        if (matchedRegex) {
+            return Maybe.Just(matchedRegex[0]);
+        } else {
+            return Maybe.Nothing;
+        }
+    }
+};
+assumption(from(/a+/iy).matchFrom("aaab")(0).withDefault("") === "aaa");
+assumption(from(/a+/iy).matchFrom("aaab")(1).withDefault("") === "aa");
+assumption(from(/a+/iy).matchFrom("aaab")(3).withDefault("") === "");
+
+
+module.exports = {
+    from
+};
