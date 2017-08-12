ebdd26cae727b22397df83fcc5eded43fccd8c70
Graeme Lockley <graemel@no9.co.za>
2017-08-12 20:19:10 +0200

    refactor: move the positive test into a single function and then simplify

--- test/ParserTest.js
+++ test/ParserTest.js
@@ -13,23 +13,26 @@ const asString = o =>
     JSON.stringify(o, null, 2);
 
 
+const assertParseInput = (input, parser, ast) => Promise
+    .resolve(LexerConfiguration.fromString(input))
+    .then(lexer => parser(lexer))
+    .then(result => Assertion
+        .isTrue(result.isOkay())
+        .equals(asString(result.content[1].result))(asString(ast))
+    );
+
+
 module.exports = Unit.Suite("Tool.SL")([
     Unit.Suite("Parser")([
         Unit.Suite("parseId")([
-            Unit.Test("hello")(Promise
-                .resolve(LexerConfiguration.fromString("hello"))
-                .then(lexer => Parser.parseId(lexer))
-                .then(result => Assertion
-                    .isTrue(result.isOkay())
-                    .equals(result.content[1].result)("hello")
-                )),
-            Unit.Test("World")(Promise
-                .resolve(LexerConfiguration.fromString("World"))
-                .then(lexer => Parser.parseId(lexer))
-                .then(result => Assertion
-                    .isTrue(result.isOkay())
-                    .equals(result.content[1].result)("World")
-                )),
+            Unit.Test("hello")(assertParseInput(
+                "hello",
+                Parser.parseId,
+                "hello")),
+            Unit.Test("World")(assertParseInput(
+                "World",
+                Parser.parseId,
+                "World")),
             Unit.Test("-error-")(Promise
                 .resolve(LexerConfiguration.fromString("-error-"))
                 .then(lexer => Parser.parseId(lexer))
@@ -40,65 +43,49 @@ module.exports = Unit.Suite("Tool.SL")([
                 ))
         ]),
         Unit.Suite("parseImport")([
-            Unit.Test("use core:Native.Data.Array:1.1.0")(Promise
-                .resolve(LexerConfiguration.fromString("use core:Native.Data.Array:1.1.0"))
-                .then(lexer => Parser.parseImport(lexer))
-                .then(result => Assertion
-                    .isTrue(result.isOkay())
-                    .equals(asString(result.content[1].result))(asString(AST.UnqualifiedImport({urn: "core:Native.Data.Array:1.1.0"})))
-                )),
-            Unit.Test("use core:Native.Data.Array:1.1.0 as Array")(Promise
-                .resolve(LexerConfiguration.fromString("use core:Native.Data.Array:1.1.0 as Array"))
-                .then(lexer => Parser.parseImport(lexer))
-                .then(result => Assertion
-                    .isTrue(result.isOkay())
-                    .equals(asString(result.content[1].result))(asString(AST.QualifiedImport({
-                        urn: "core:Native.Data.Array:1.1.0",
-                        name: "Array"
-                    })))
-                )),
-            Unit.Test("use core:Native.Data.Array:1.1.0 import length")(Promise
-                .resolve(LexerConfiguration.fromString("use core:Native.Data.Array:1.1.0 import length"))
-                .then(lexer => Parser.parseImport(lexer))
-                .then(result => Assertion
-                    .isTrue(result.isOkay())
-                    .equals(asString(result.content[1].result))(asString(AST.QualifiedNameImport({
-                        urn: "core:Native.Data.Array:1.1.0",
-                        names: [{name: "length", qualified: Maybe.Nothing}]
-                    })))
-                )),
-            Unit.Test("use core:Native.Data.Array:1.1.0 import length as arrayLength")(Promise
-                .resolve(LexerConfiguration.fromString("use core:Native.Data.Array:1.1.0 import length as arrayLength"))
-                .then(lexer => Parser.parseImport(lexer))
-                .then(result => Assertion
-                    .isTrue(result.isOkay())
-                    .equals(asString(result.content[1].result))(asString(AST.QualifiedNameImport({
-                        urn: "core:Native.Data.Array:1.1.0",
-                        names: [{name: "length", qualified: Maybe.Just("arrayLength")}]
-                    })))
-                )),
-            Unit.Test("use core:Native.Data.Array:1.1.0 import (length, substring as subs)")(Promise
-                .resolve(LexerConfiguration.fromString("use core:Native.Data.Array:1.1.0 import (length, substring as subs)"))
-                .then(lexer => Parser.parseImport(lexer))
-                .then(result => Assertion
-                    .isTrue(result.isOkay())
-                    .equals(asString(result.content[1].result))(asString(AST.QualifiedNameImport({
-                        urn: "core:Native.Data.Array:1.1.0",
-                        names: [
-                            {name: "length", qualified: Maybe.Nothing},
-                            {name: "substring", qualified: Maybe.Just("subs")}
-                        ]
-                    })))
-                ))
+            Unit.Test("use core:Native.Data.Array:1.1.0")(assertParseInput(
+                "use core:Native.Data.Array:1.1.0",
+                Parser.parseImport,
+                AST.UnqualifiedImport({urn: "core:Native.Data.Array:1.1.0"}))),
+            Unit.Test("use core:Native.Data.Array:1.1.0 as Array")(assertParseInput(
+                "use core:Native.Data.Array:1.1.0 as Array",
+                Parser.parseImport,
+                AST.QualifiedImport({
+                    urn: "core:Native.Data.Array:1.1.0",
+                    name: "Array"
+                }))),
+            Unit.Test("use core:Native.Data.Array:1.1.0 import length")(assertParseInput(
+                "use core:Native.Data.Array:1.1.0 import length",
+                Parser.parseImport,
+                AST.QualifiedNameImport({
+                    urn: "core:Native.Data.Array:1.1.0",
+                    names: [{name: "length", qualified: Maybe.Nothing}]
+                }))),
+            Unit.Test("use core:Native.Data.Array:1.1.0 import length as arrayLength")(assertParseInput(
+                "use core:Native.Data.Array:1.1.0 import length as arrayLength",
+                Parser.parseImport,
+                AST.QualifiedNameImport({
+                    urn: "core:Native.Data.Array:1.1.0",
+                    names: [{name: "length", qualified: Maybe.Just("arrayLength")}]
+                }))),
+            Unit.Test("use core:Native.Data.Array:1.1.0 import (length, substring as subs)")(assertParseInput(
+                "use core:Native.Data.Array:1.1.0 import (length, substring as subs)",
+                Parser.parseImport,
+                AST.QualifiedNameImport({
+                    urn: "core:Native.Data.Array:1.1.0",
+                    names: [
+                        {name: "length", qualified: Maybe.Nothing},
+                        {name: "substring", qualified: Maybe.Just("subs")}
+                    ]
+                })))
         ]),
         Unit.Suite("parseTypeReference2")([
-            Unit.Test("Int")(Promise
-                .resolve(LexerConfiguration.fromString("Int"))
-                .then(lexer => Parser.parseTypeReference2(lexer))
-                .then(result => Assertion
-                    .isTrue(result.isOkay())
-                    .equals(asString(result.content[1].result))(asString(AST.Int))
-                ))
+            Unit.Test("Int")(assertParseInput(
+                "Int",
+                Parser.parseTypeReference2,
+                AST.Int))
         ])
     ])
 ]);
+
+
