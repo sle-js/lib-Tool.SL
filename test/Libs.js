c7324e73877a870df4c68085009954d82561192a
Graeme Lockley <graemel@no9.co.za>
2017-09-02 07:01:20 +0200

    chore: upgrade assertion

--- test/Libs.js
+++ test/Libs.js
@@ -2,6 +2,6 @@ const Libs = require("../src/Libs");
 
 
 module.exports = Object.assign({}, Libs, {
-    Assertion: mrequire("core:Test.Unit.Assertion:2.0.0"),
+    Assertion: mrequire("core:Test.Unit.Assertion:2.0.1"),
     Unit: mrequire("core:Test.Unit:1.0.0")
 });
