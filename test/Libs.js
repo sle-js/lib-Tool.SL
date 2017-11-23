f2001ac67bc1e042fabda1a92ea961b7e3ee80ea
Graeme Lockley <graemel@no9.co.za>
2017-11-23 20:52:44 +0200

    refactor: change the test Libs into promise style

--- test/Libs.js
+++ test/Libs.js
@@ -1,15 +1,24 @@
-const Libs = require("../src/Libs");
-
-
-module.exports = Object.assign({}, Libs, {
-    Assertion: mrequire("core:Test.Unit.Assertion:2.0.1"),
-    AST: require("../src/AST"),
-    Lexer: require("../src/Lexer"),
-    LexerConfiguration: require("../src/LexerConfiguration"),
-    Parser: require("../src/Parser"),
-    Path: require("path"),
-    Regex: require("../src/Regex"),
-    Tokens: require("../src/Tokens"),
-    Translator: require("../src/Translator"),
-    Unit: mrequire("core:Test.Unit:1.0.0")
-});
+module.exports = $importAll([
+    "../src/Libs",
+    "core:Test.Unit.Assertion:2.0.1",
+    "../src/AST",
+    "../src/Lexer",
+    "../src/LexerConfiguration",
+    "../src/Parser",
+    "path",
+    "../src/Regex",
+    "../src/Tokens",
+    "../src/Translator",
+    "core:Test.Unit:1.0.0"
+]).then($imports => Object.assign({}, $imports[0], {
+    Assertion: $imports[1],
+    AST: $imports[2],
+    Lexer: $imports[3],
+    LexerConfiguration: $imports[4],
+    Parser: $imports[5],
+    Path: $imports[6],
+    Regex: $imports[7],
+    Tokens: $imports[8],
+    Translator: $imports[9],
+    Unit: $imports[10]
+}));
