016ea820f42237c288f91f2839dd4d856b2abe64
Graeme Lockley <graemel@no9.co.za>
2017-11-23 06:06:02 +0200

    refactor: move to promise module style

--- test/FloatTest.js
+++ test/FloatTest.js
@@ -1,18 +1,20 @@
-const Assertion = require("./Libs").Assertion;
-const Unit = require("./Libs").Unit;
+module.exports = $import(
+    "./Libs"
+).then($imports => {
+    const Assertion = $imports.Assertion;
+    const Unit = $imports.Unit;
+    const Float = $imports.Float;
 
-const Float = require("./Libs").Float;
-
-
-module.exports = Unit.Suite("Native.Data.Float")([
-    Unit.Suite("fromString")([
-        Unit.Test("'123.0' equals 123.0")(Assertion
-            .equals(Float.fromString("123.0").withDefault(0.0))(123.0)),
-        Unit.Test("'12abc' equals 12.0")(Assertion
-            .equals(Float.fromString("12abc").withDefault(0.0))(12.0)),
-        Unit.Test("'abc' with default -10.0 equals -10.0")(Assertion
-            .equals(Float.fromString("abc").withDefault(-10.0))(-10.0)),
-        Unit.Test("'.123' equals 0.123")(Assertion
-            .equals(Float.fromString(".123").withDefault(0.0))(0.123))
-    ])
-]);
+    return Unit.Suite("Native.Data.Float")([
+        Unit.Suite("fromString")([
+            Unit.Test("'123.0' equals 123.0")(Assertion
+                .equals(Float.fromString("123.0").withDefault(0.0))(123.0)),
+            Unit.Test("'12abc' equals 12.0")(Assertion
+                .equals(Float.fromString("12abc").withDefault(0.0))(12.0)),
+            Unit.Test("'abc' with default -10.0 equals -10.0")(Assertion
+                .equals(Float.fromString("abc").withDefault(-10.0))(-10.0)),
+            Unit.Test("'.123' equals 0.123")(Assertion
+                .equals(Float.fromString(".123").withDefault(0.0))(0.123))
+        ])
+    ]);
+});
