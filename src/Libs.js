12d849abb7f69f81be7131c9a77606e561be0206
Graeme Lockley <graemel@no9.co.za>
2017-11-23 20:43:26 +0200

    refactor: sort imports alphabetically

--- src/Libs.js
+++ src/Libs.js
@@ -3,7 +3,7 @@ module.exports = {
     FileSystem: mrequire("core:Native.System.IO.FileSystem:1.1.0"),
     Float: require("./Float"),
     Int: mrequire("core:Native.Data.Int:1.0.0"),
-    Stream: mrequire("core:Data.Collection.InfiniteStream:1.0.0"),
     Maybe: mrequire("core:Native.Data.Maybe:1.0.0"),
+    Stream: mrequire("core:Data.Collection.InfiniteStream:1.0.0"),
     String: mrequire("core:Native.Data.String:1.0.0")
 };
