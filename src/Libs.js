310187e8f366abae4bc1bb22991af9371391c80a
Graeme Lockley <graemel@no9.co.za>
2017-11-23 20:35:21 +0200

    refactor: use core:Native.Data.String:1.0.0

--- src/Libs.js
+++ src/Libs.js
@@ -5,5 +5,5 @@ module.exports = {
     Int: mrequire("core:Native.Data.Int:1.0.0"),
     Stream: mrequire("core:Data.Collection.InfiniteStream:1.0.0"),
     Maybe: mrequire("core:Native.Data.Maybe:1.0.0"),
-    String: require("./String")
+    String: mrequire("core:Native.Data.String:1.0.0")
 };
