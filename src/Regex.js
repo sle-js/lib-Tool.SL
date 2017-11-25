def035aa136f11ff316e8b1fc1721ccaf6265a95
Graeme Lockley <graemel@no9.co.za>
2017-11-25 05:24:56 +0200

    refactor: move to promise module style

--- src/Regex.js
+++ src/Regex.js
@@ -1,34 +1,38 @@
-const Maybe = require("./Libs").Maybe;
+module.exports = $import(
+    "./Libs"
+).then($imports => {
+    const Maybe = $imports.Maybe;
 
 
-function Regex(regex) {
-    this.regex = regex;
-}
-
-
-function from(regex) {
-    return new Regex(regex);
-}
-
-
-Regex.prototype.matchFrom = function (input) {
-    return index => {
-        this.regex.lastIndex = index;
-        const matchedRegex = this.regex.exec(input);
-        if (matchedRegex) {
-            return Maybe.Just(matchedRegex[0]);
-        } else {
-            return Maybe.Nothing;
-        }
+    function Regex(regex) {
+        this.regex = regex;
     }
-};
-assumption(from(/a+/iy).matchFrom("aaab")(0).withDefault("") === "aaa");
-assumption(from(/a+/iy).matchFrom("aaab")(1).withDefault("") === "aa");
-assumption(from(/a+/iy).matchFrom("aaab")(3).withDefault("") === "");
-assumption(from(/[a-z]+/y).matchFrom("--Hello--")(2).withDefault("") === "");
-assumption(from(/[a-z]+/y).matchFrom("--hello--")(2).withDefault("") === "hello");
 
 
-module.exports = {
-    from
-};
+    function from(regex) {
+        return new Regex(regex);
+    }
+
+
+    Regex.prototype.matchFrom = function (input) {
+        return index => {
+            this.regex.lastIndex = index;
+            const matchedRegex = this.regex.exec(input);
+            if (matchedRegex) {
+                return Maybe.Just(matchedRegex[0]);
+            } else {
+                return Maybe.Nothing;
+            }
+        }
+    };
+    assumption(from(/a+/iy).matchFrom("aaab")(0).withDefault("") === "aaa");
+    assumption(from(/a+/iy).matchFrom("aaab")(1).withDefault("") === "aa");
+    assumption(from(/a+/iy).matchFrom("aaab")(3).withDefault("") === "");
+    assumption(from(/[a-z]+/y).matchFrom("--Hello--")(2).withDefault("") === "");
+    assumption(from(/[a-z]+/y).matchFrom("--hello--")(2).withDefault("") === "hello");
+
+
+    return {
+        from
+    };
+});
