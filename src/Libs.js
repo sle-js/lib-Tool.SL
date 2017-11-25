dc6b12d2e3b5218de123975a93192fa0370dae1d
Graeme Lockley <graemel@no9.co.za>
2017-11-25 05:30:20 +0200

    refactor: move to promise module style

--- src/Libs.js
+++ src/Libs.js
@@ -1,11 +1,22 @@
-module.exports = {
-    Array: mrequire("core:Native.Data.Array:1.2.0"),
-    FileSystem: mrequire("core:Native.System.IO.FileSystem:1.1.0"),
-    Float: require("./Float"),
-    Int: mrequire("core:Native.Data.Int:1.0.0"),
-    Lexer: mrequire("core:Text.Parsing.Lexer:1.0.0"),
-    Maybe: mrequire("core:Native.Data.Maybe:1.0.0"),
-    Result: mrequire("core:Native.Data.Result:1.0.0"),
-    Stream: mrequire("core:Data.Collection.InfiniteStream:1.0.0"),
-    String: mrequire("core:Native.Data.String:1.0.0")
-};
+module.exports = $importAll([
+    "core:Native.Data.Array:1.2.0",
+    "core:Native.System.IO.FileSystem:1.1.0",
+    "./Float",
+    "core:Native.Data.Int:1.0.0",
+    "core:Text.Parsing.Lexer:1.0.0",
+    "core:Native.Data.Maybe:1.0.0",
+    "core:Native.Data.Result:1.0.0",
+    "core:Data.Collection.InfiniteStream:1.0.0",
+    "core:Native.Data.String:1.0.0"
+
+]).then($imports => ({
+    Array: $imports[0],
+    FileSystem: $imports[1],
+    Float: $imports[2],
+    Int: $imports[3],
+    Lexer: $imports[4],
+    Maybe: $imports[5],
+    Result: $imports[6],
+    Stream: $imports[7],
+    String: $imports[8]
+}));
