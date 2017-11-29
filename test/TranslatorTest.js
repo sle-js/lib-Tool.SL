3936ec5302ddde10c84b275cfb1376ede387c389
Graeme Lockley <graemel@no9.co.za>
2017-11-29 06:24:45 +0200

    fix: change the toString so that it can also accept string values

--- test/TranslatorTest.js
+++ test/TranslatorTest.js
@@ -14,7 +14,9 @@ module.exports = $import(
 
 
     const asString = o =>
-        JSON.stringify(o, null, 2);
+        typeof o === "string"
+            ? o
+            : JSON.stringify(o, null, 2);
 
 
     const parseFile = content => {
@@ -81,8 +83,8 @@ module.exports = $import(
                 Parser[parseName](LexerConfiguration.fromString(content.src.join("\n")));
 
             return assertion
-                        .isTrue(ast.isOkay())
-                        .equals(asString(ast.content[1].result).trim())(content[astKey.withDefault("ast parseModule")].join("\n").trim());
+                .isTrue(ast.isOkay())
+                .equals(asString(ast.content[1].result).trim())(content[astKey.withDefault("ast parseModule")].join("\n").trim());
         } else {
             return assertion;
         }
